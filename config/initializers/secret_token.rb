b3c0d3a2e6cfe52138bd995c907aba29bfbfe1da
Padi Ignacio <marcrendlignacio@gmail.com>
2012-02-19 16:39:25 +0800

    Updated Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pabilist::Application.config.secret_token = 'ade40c8c1be180d54f7354a748cadbdde4fec84b57b4991acbcaa9fde6ac28b4f53a634679d6d93eaffdfe5032c43ea5e6080bc1cbd90f21f6e2fc5952ad9142'
